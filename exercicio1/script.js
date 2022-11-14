8d77e5ce784af7e6cb934e1713ab62debbf29a48
Anderson-Schi <jr93@hotmai.com>
2022-11-14 21:31:58 +0000

    exercicios resolvidos

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,2 +1,10 @@
-console.log("teste 1")
+// Crie um código que receba um número por prompt e verifique se um número é divisível por 2 ou por 3
+const num1 = prompt("Digite um numero: ");
+const rNum1 = Number(num1);
 
+if((rNum1%2) === 0 ||(rNum1%3) === 0){
+    console.log(`O numero digitado ${rNum1} e divisivel por 2 ou 3`)
+
+}else{
+    console.log(`O numero digitado ${rNum1} nao e divisivel por 2 ou 3`)
+};
